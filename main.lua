283bf3dc441e3277a10c04065b819f139030f20a
Vladislav Morozov <ladniy101@gmail.com>
2023-10-18 16:53:20 +0800

    0.0.1 commit

--- main.lua
+++ main.lua
@@ -1,11 +1,11 @@
-require 'objects/player'
+require 'objects/character'
 require 'objects/enemy'
 
 function love.load()
   ArenaWidth = 800
   ArenaHeight = 600
 
-  PlayerOne = Player:new(ArenaWidth / 2, ArenaHeight / 2 + 200, 100)
+  Player = Character:new(ArenaWidth / 2, ArenaHeight / 2 + 200, 100, 10)
 
   Enemies = {
     Enemy:new(ArenaWidth / 2 - 150, ArenaHeight * 0.20, 100, 2, false),
@@ -13,68 +13,122 @@ function love.load()
     Enemy:new(ArenaWidth / 2 + 150, ArenaHeight * 0.20, 100, 1, false)
   }
 
-  EnemyCount = 3
+  CurrentDelta = 0
 
-  function EnemiesAttack()
-    EnemySumOfAttack = 0
-  	if EnemyCount ~= 0 then
+  function GetItemsCount(table)
+    local count = 0
+    for itemIndex, item in pairs(table) do
+    	count = count + 1
+    end
+
+    return count
+  end
+
+  function EnemiesAttack(object)
+    local sumOfDamage = 0
+  	if GetItemsCount(Enemies) ~= 0 then
       for enemyIndex, enemy in pairs(Enemies) do
-      	EnemySumOfAttack = EnemySumOfAttack + enemy:getAttack()
+      	sumOfDamage = sumOfDamage + enemy:getDamage()
       end
-    	PlayerOne.hp = PlayerOne.hp - EnemySumOfAttack
+    	object.health = object.health - sumOfDamage
     end
   end
 
   function SetRandomTarget(table)
-    RandomIndex = love.math.random(1, EnemyCount)
+    local randomIndex = love.math.random(1, GetItemsCount(Enemies))
     for itemIndex, item in pairs(table) do
     	if item:getIsTarget() == true then
       	item.target = false
       end
-      table[RandomIndex].target = true
+      table[randomIndex].target = true
     end
   end
 
-  CurrentDelta = 0
+  function MoveToLeft(player, enemies)
+    local currentX = player:getX()
+  	local leftX = 0
+    for itemIndex, item in pairs(enemies) do
+      if item:getX() < currentX then
+      	leftX = item:getX()
+      end
+    end
+    player.x = leftX
+  end
+
+  function MoveToRight(player, enemies)
+    local currentX = player:getX()
+  	local rightX = 0
+    for itemIndex, item in pairs(enemies) do
+      if item:getX() > currentX then
+      	rightX = item:getX()
+      end
+    end
+    player.x = rightX
+  end
 end
 
 function love.keypressed(key)
-  if key == 'space' then
-    for enemyIndex, enemy  in pairs(Enemies) do
+  if key == 'left' then
+  	MoveToLeft(Player, Enemies)
+  end
+
+  if key == 'right' then
+  	MoveToRight(Player, Enemies)
+  end
+end
+
+function love.mousepressed(x, y, button)
+  if button == 1 then
+  	for enemyIndex, enemy  in pairs(Enemies) do
     	if enemy:getIsTarget() == true then
-      	enemy.hp = enemy.hp - 10
+      	enemy.health = enemy.health - 10
       end
     end
   end
 end
 
 function love.draw()
-  -- Draw a player
-  love.graphics.setColor(0, 0, 1)
-  love.graphics.circle('fill', PlayerOne:getX(), PlayerOne:getY(), 30)
-  love.graphics.setColor(0, 1, 0)
-  love.graphics.rectangle('fill', PlayerOne:getX() - 25, PlayerOne:getY() + 40, PlayerOne:getHP() * 0.5, 10)
-  love.graphics.setColor(1, 1, 1)
-  love.graphics.print('HP: '..PlayerOne:getHP(), PlayerOne:getX() - 25, PlayerOne:getY() + 40)
+  -- Player drawning
+  if Player:getHealth() > 0 then
+    -- Draw a player
+  	love.graphics.setColor(0, 0, 1)
+    love.graphics.circle('fill', Player:getX(), Player:getY(), 30)
 
+    -- Draw a health bar
+    love.graphics.setColor(0, 1, 0)
+    love.graphics.rectangle('fill', Player:getX() - 25, Player:getY() + 40, Player:getHealth() * 0.5, 10)
+
+    -- Print a health points
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.print('HP: '..Player:getHealth(), Player:getX() - 25, Player:getY() + 40)
+  end
+
+  -- Enemy drawning
   for enemyIndex, enemy in pairs(Enemies) do
     -- Draw each enemy
   	love.graphics.setColor(1, 0, 0)
     love.graphics.circle('fill', enemy:getX(), enemy:getY(), 30)
 
-    -- Draw hp bar under each enemy
+    -- Draw a health bar for each enemy
     love.graphics.setColor(0, 1, 0)
-    love.graphics.rectangle('fill', enemy:getX() - 25, enemy:getY() + 40, enemy:getHP() * 0.5, 10)
+    love.graphics.rectangle('fill', enemy:getX() - 25, enemy:getY() + 40, enemy:getHealth() * 0.5, 10)
 
-    -- Draw hp count on hp bar
+    -- Print a health points for each enemy
     love.graphics.setColor(1, 1, 1)
-    love.graphics.print('HP: '..enemy:getHP(), enemy:getX() - 25, enemy:getY() + 40)
+    love.graphics.print('HP: '..enemy:getHealth(), enemy:getX() - 25, enemy:getY() + 40)
   end
 
-    -- Debug
+  -- Print a "GAME OVER" if player died
+  if Player:getHealth() < 0 then
+  	love.graphics.setColor(1, 0, 0)
+    love.graphics.print('GAME OVER', ArenaHeight / 2, ArenaWidth / 2)
+  end
+
+    -- Debug info
+    love.graphics.setColor(1, 1, 1)
     love.graphics.print(table.concat({
-    'random: '..RandomIndex,
-    'dt: '..CurrentDelta
+    'dt: '..CurrentDelta,
+    'fps: '..love.timer.getFPS()
   },'\n'))
 end
 
@@ -83,14 +137,13 @@ function love.update(dt)
 
   -- Remove enemy if enemy hp below 0
   for enemyIndex, enemy in pairs(Enemies) do
-  	if enemy:getHP() <= 0 then
+  	if enemy:getHealth() <= 0 then
     	table.remove(Enemies, enemyIndex)
-      EnemyCount = EnemyCount - 1
     end
   end
 
   CurrentDelta = math.ceil(CurrentDelta + (dt * 100))
   if CurrentDelta % 100 == 1 then
-  	EnemiesAttack()
+  	EnemiesAttack(Player)
   end
 end
--- objects/character.lua
+++ objects/character.lua
@@ -1,13 +1,12 @@
-Enemy = {}
+Character = {}
 
-function Enemy:new(x, y, health, damage, target)
+function Character:new(x, y, health, damage)
 
   local obj = {}
     obj.x = x
     obj.y = y
     obj.health = health
     obj.damage = damage
-    obj.target = target
 
   function obj:getX()
   	return self.x
@@ -23,10 +22,6 @@ function Enemy:new(x, y, health, damage, target)
 
   function obj:getDamage()
   	return self.damage
-  end
-
-  function obj:getIsTarget()
-  	return self.target
   end
 
   setmetatable(obj, self)
