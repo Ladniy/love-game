460e7f36b47b582d8abee1543a63cb6131c2f1cb
Ladniy <ladniy101@gmail.com>
2023-11-02 23:18:44 +0800

    feat: move utils function in independent file

--- main.lua
+++ main.lua
@@ -15,9 +15,6 @@ function love.load()
     Weapon:new('Knuckle', 2, 1/2)
   }
 
-  function Random(min, max)
-    return love.math.random(min, max)
-  end
 
   Player = Character:new(ArenaWidth / 2, ArenaHeight / 2 + 200, 100, WeaponsTable[Random(1, 4)])
 
@@ -29,20 +26,7 @@ function love.load()
 
   Score = 0
 
-  function DrawCenteredText(rectX, rectY, rectWidth, rectHeight, text)
-	  local font       = love.graphics.getFont()
-	  local textWidth  = font:getWidth(text)
-	  local textHeight = font:getHeight()
-	  love.graphics.print(text, rectX+rectWidth/2, rectY+rectHeight/2, 0, 3, 3, textWidth/2, textHeight/2)
-  end
 
-  function GetItemsCount(table)
-    local count = 0
-    for itemIndex, item in pairs(table) do
-    	count = count + 1
-    end
-    return count
-  end
 
   function EnemiesAttack(player, damage)
     player.health = player.health - damage
